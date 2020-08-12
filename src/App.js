7a02472a95b034205d753a53bd83091390364b71
chris_sted <christopher.stedman@students.mq.edu.au>
2020-08-12 15:13:27 +1000

    - Added json-server - Implemented state for users & posts - Implemented json-server GET requests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [users, setUsers] = useState([])
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/users')
+    .then(response => {
+      setUsers(response.data)
+    })
+  }, [])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/posts')
+    .then(response => {
+      setPosts(response.data)
+    })
+  },[])
+
+return(
+  users.map(u => <p>{u.id}</p>)
+)
 }
 
 export default App;
